525716e667cdde0d33d98311ee640cad5ac1f2bd
ilight1542 <ilight1542@gmail.com>
2022-01-25 13:52:22 +0100

    updated species counting

--- brucella/genome_metadata_analysis.sh
+++ brucella/genome_metadata_analysis.sh
@@ -1,3 +1,7 @@
+## dataset comes from 
+https://www.ncbi.nlm.nih.gov/datasets/genomes/?taxon=234&utm_source=genome&utm_medium=referral
+## NCBI genomes database
+
 ## number of unique species
 grep "Brucella" brucella_genomes_metadata.tsv | awk -F'\t' 'NR > 1 {print $1}' | awk '{print $1, $2}' | sort | uniq > brucella_species.txt
 
@@ -23,5 +27,11 @@ done
 
 while read line; do
     count=$(grep -c "$line" brucella_genomes_metadata_chorm_compl.tsv)
-    echo "$line" $count >> brucella_species_counts_chrom_compl.txt
+    echo "$line", $count >> brucella_species_counts.txt
+done <brucella_species.txt
+
+
+while read line; do
+    count=$(grep -c "$line" brucella_genomes_metadata_chorm_compl.tsv.filtered)
+    echo "$line", $count  >> brucella_species_counts_filtered.txt
 done <brucella_species.txt
