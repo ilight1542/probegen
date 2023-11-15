781a796ec133af66929532312405801dbad7fb50
ilight1542 <ilight1542@gmail.com>
2023-11-15 13:45:55 +0100

    updated for testing suite

--- probegen.sh
+++ probegen.sh
@@ -22,15 +22,109 @@ maskedthreshold="10"
 
 ## functions
 
-usage() { # Function: Print a help message.
-  printf "Usage: $script_name [-O <OUTPUT DIR>] [-I </path/to/folder/with/eager/loops> ] [-n </path/to/malt/flagging/list.txt>, see postprocessing.AMPS ] [-d <damage cutoff on first/last read>, see postprocessing.AMPS] [-e <default read ratio>, see postprocessing.AMPS] [-a <ancient read ratio>, see postprocessing.AMPS] [-s <Sequencing strategy (PE vs SE)>, see postprocessing.AMPS] [-t <NUM_THREADS>] -h(elp) -v(erbose) -V(ersion)" 1>&2 
-  # Will make output NEXT to rma6 file supplying -r will only export major ranks
-}
-
-exit_abnormal() { # print usage and exit
-    usage
+# Function to display script usage
+usage() {
+    echo "Usage: $0 -I <file> -O <file> [options]"
+    echo "Mandatory parameters:"
+    echo "  -I, --genome-paths-file <file>                   Set the genome paths file (mandatory)"
+    echo "  -O, --output <file>                              Set the output file (mandatory)"
+    echo "Options:"
+    echo "  -b, --percent-ambiguous-base-threshold <value>   Set the percent ambiguous base threshold for genome inclusion (default: 1)"
+    echo "  -M, --run-masking                                Enable masking (default: true)"
+    echo "  -l, --length <value>                             Set the length for probes (default: 52)"
+    echo "  -s, --step-size <value>                          Set the step size for probes (default: 1)"
+    echo "  -m, --masked-threshold <value>                   Set the masked threshold for probe inclusion (default: 10)"
+    echo "  -r, --randseed <value>                           Set the random seed for probe generation (default: 100)"
+    echo "  -C, --run-clustering                             Enable clustering (default: true)"
+    echo "  -i, --minimum-percent-identity <value>           Set the minimum percent identity for clustering (default: 95)"
+    echo "  -t, --max-terminal-mismatches <value>            Set the max terminal mismatches for clustering (default: 2)"
+    echo "  -a, --adapter-seq <seq>                          Set the adapter sequence for appending to probes (default: none)"
+    echo "  -v, --version                                    Show version"
+    echo "  -h, --help                                       Show help"
     exit 1
 }
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -I|--genome-paths-file)
+            genomepathsfile="$2"
+            shift
+            shift
+            ;;
+        -O|--output)
+            output="$2"
+            shift
+            shift
+            ;;
+        -b|--percent-ambiguous-base-threshold)
+            percentambiguousbasethreshold="$2"
+            shift
+            shift
+            ;;
+        -M|--run-masking)
+            runmasking=true
+            shift
+            ;;
+        -l|--length)
+            length="$2"
+            shift
+            shift
+            ;;
+        -s|--step-size)
+            stepsize="$2"
+            shift
+            shift
+            ;;
+        -m|--masked-threshold)
+            maskedthreshold="$2"
+            shift
+            shift
+            ;;
+        -r|--randseed)
+            randseed="$2"
+            shift
+            shift
+            ;;
+        -C|--run-clustering)
+            runclustering=true
+            shift
+            ;;
+        -i|--minimum-percent-identity)
+            minimumpercentidentity="$2"
+            shift
+            shift
+            ;;
+        -t|--max-terminal-mismatches)
+            maxterminalmismatches="$2"
+            shift
+            shift
+            ;;
+        -a|--adapter-seq)
+            adapterseq="$2"
+            shift
+            shift
+            ;;
+        -v|--version)
+            version=true
+            shift
+            ;;
+        -h|--help)
+            help=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $key"
+            usage
+            ;;
+    esac
+done
+
+# Check if mandatory options are provided
+if [[ -z $genomepathsfile ]] || [[ -z $output ]]; then
+    echo "Error: Mandatory options -I and -O must be provided."
+    usage
+fi
 
 help() { # print help, explanation for all parameters
     printf "
@@ -53,93 +147,54 @@ help() { # print help, explanation for all parameters
 
         Optional:
         -b
-            --percentambiguousbasethreshold. Default=1
+            --percent-ambiguous-base-threshold. Default=1
         -M
-            --rundustmasker - Turn on dustmasker of low-complexity regions. Default=true
+            --run-dustmasker - Turn on dustmasker of low-complexity regions. Default=true
         -l
-            --length - length of probes to generate (without adapter). Default=52
+            --length - Length of probes to generate (without adapter). Default=52
         -s
-            --stepsize - stepsize of probes to generate. Default=0
+            --step-size - Step size of probes to generate. Default=0
+        -m
+            --masked-threshold - Maximum number of reads in a probe that can be masked by dustmasker before the probe is not considered. Default=10
         -r
             --randseed - Seed for random pick of replacement of non-ATCG bases in probe. Default=100
         -C
-            --runclustering - Run clustering of similar probes using cd-hit-est. Default=true
+            --run-clustering - Run clustering of similar probes using cd-hit-est. Default=true
         -i
-            --minimumpercentidentity - Minimum percent identity on aligned region for cd-hit-est to cluster a probe with a representative. Default=95
+            --minimum-percent-identity - Minimum percent identity on aligned region for cd-hit-est to cluster a probe with a representative. Default=95
         -t
-            --maxterminalmismatches - Maximum number of terminal mismatches (leading or tail) for cd-hit-est to consider an alignment. Default=2
-        -m
-            --maskedthreshold - Maximum number of reads in a probe that can be masked by dustmasker before the probe is not considered. Default=10
+            --max-terminal-mismatches - Maximum number of terminal mismatches (leading or tail) for cd-hit-est to consider an alignment. Default=2
         -a
-            --adapterseq - Adaptersequence to be appended to all probes. No default.
-        -h      
-            print this help message
+            --adapter-seq - Adaptersequence to be appended to all probes. No default.
+        -h     
+            --help - Print this help message
         -v      
-            make execution verbose
-        -V      
-            print version
+            --version - Print version
 
     AUTHOR
         Ian Light-Maka (ilight1542@gmail.com)
 
     VERSION
-
         ${prog_version}
     \n    
     "
 }
 
-## argument parsing ## 
-while getopts ":I:ObMlsrCitmavh" options; do         # Loop: Get the next option;
-                                          # use silent error checking
-  case "${options}" in                    # 
-    I)genomepathsfile=${OPTARG};;
-    O)output=${OPTARG};;
-    b)percentambiguousbasethreshold=${OPTARG};;
-    M)runmasking=true;;
-    l)length=${OPTARG};;
-    s)stepsize=${OPTARG};;
-    r)randseed=${OPTARG};;
-    C)runclustering=true;;
-    i)minimumpercentidentity=${OPTARG};;
-    t)maxterminalmismatches=${OPTARG};;
-    m)maskedthreshold=${OPTARG};;
-    a)adapterseq=${OPTARG};;
-    v)version=true;;
-    h)help=true;;
-    :)                                    # If expected argument omitted:
-      echo "Error: -${OPTARG} requires an argument."
-      exit_abnormal                       # Exit abnormally.
-      ;;
-    *)                                    # If unknown (any other) option:
-      exit_abnormal                       # Exit abnormally.
-      ;;
-  esac
-done
-
 ## validation ##
-
 if [[ $version == true ]]; then
   printf $prog_version
   exit 0
 fi
 
-if [[ $OPTIND -eq 1 ]]; then echo "No options were passed!" && exit_abnormal; fi
-shift $((OPTIND-1))
-
 if [[ $help == true ]]; then help && exit 0; fi
 
-
 # BEGIN SCRIPT 
-if [[ ! ${genomepathsfile} ]] ; then
-    printf "Predownloaded genome paths file must be given! Please supply a text file with a path to each reference genome"
-    exit 1
-fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Check all file paths
 date=$(date)
 echo "PROBEGEN - ${date}: Running pipeline checker"
-if ! python3 pipeline_checker.py -i ${genomepathsfile} ; then
+if ! python3 ${SCRIPT_DIR}/pipeline_checker.py -i ${genomepathsfile} --stepsize ${stepsize} --length ${length} --maxterminalmismatches ${maxterminalmismatches} --maskedthreshold ${maskedthreshold} --minimumpercentidentity ${minimumpercentidentity} --percentambiguousbasethreshold ${percentambiguousbasethreshold}; then
     exit 1
 fi
 
@@ -170,14 +225,14 @@ if ( ${runmasking} ) ; then
     date=$(date)
     echo "     - ${date}: Running clustering"
     if ( ${runclustering} ) ; then
-        python3 tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
+        python3 ${SCRIPT_DIR}/tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
             --masked_regions dusted_genomes.fasta --masked_cutoff ${maskedthreshold}\
             --out_name tiling_out.txt
         file_to_add_apaters=${output}_cdhit.fasta
-        python3 execute_clustering.py -f tiling_out.txt -o ${output}_cdhit.fasta -l ${length} -i ${minimumpercentidentity} -t ${maxterminalmismatches}
+        python3 ${SCRIPT_DIR}/execute_clustering.py -f tiling_out.txt -o ${output}_cdhit.fasta -l ${length} -i ${minimumpercentidentity} -t ${maxterminalmismatches}
     else
         # check if reverse complement is already present in growing probe set
-        python3 tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
+        python3 ${SCRIPT_DIR}/tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
             --masked_regions dusted_genomes.fasta --masked_cutoff ${maskedthreshold}\
             --reverse_complement --out_name tiling_out.txt
         file_to_add_apaters=tiling_out.txt
@@ -186,13 +241,13 @@ else
     date=$(date)
     echo "     - ${date}: Running clustering"
     if ( ${runclustering} ) ; then
-        python3 tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
+        python3 ${SCRIPT_DIR}/tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
             --out_name tiling_out.txt
         file_to_add_apaters=${output}_cdhit.fasta
         python3 execute_clustering.py -f tiling_out.txt -o ${file_to_add_apaters} -l ${length} -i ${minimumpercentidentity} -t ${maxterminalmismatches}
     else
             # check if reverse complement is already present in growing probe set
-        python3 tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
+        python3 ${SCRIPT_DIR}/tiling.py -i final_genomes.txt -l ${length} -s ${stepsize} --randseed ${randseed}\
             --out_name tiling_out.txt
         file_to_add_apaters=tiling_out.txt
     fi
